1839f2a5420f9f4f110d33a4c052d4174476348c
Christopher <christopherstoy@gmail.com>
2020-09-01 00:25:18 +0000

    added new web form using body-parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,11 +32,28 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok");
+});
+
+function generateRandomString() {
+ 
+  let randomString = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+
+return randomString;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
