d2c3d68cf659952aa59fbca3432adb6555057ad0
Christopher <christopherstoy@gmail.com>
2020-09-01 19:55:34 +0000

    updated express_server

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
